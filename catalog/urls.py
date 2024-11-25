6a77a8ef2fdad791e5860db6465c1850c2618e79
Даниил Ефремов <de312451@gmail.com>
2024-11-25 21:28:07 +0300

    Homework done

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('contact/', ContactView.as_view(), name='contact'),
     path('product/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
     path('add-product/', AddProductView.as_view(), name='add_product'),
+    path('update/<int:product_id>/', UpdateProductView.as_view(), name='update_product'),
+    path('delete/<int:product_id>/', DeleteProductView.as_view(), name='delete_product'),
 ]
+
 
